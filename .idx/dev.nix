f8d180641538303ffa065819e2b67ec5dd757b03
thomasjrlogan <thomasjrlogan@gmail.com>
2025-09-20 16:02:30 +0000

    Add API key and .gitignore

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -3,6 +3,9 @@
   packages = [
     pkgs.nodejs_20
   ];
+  env = {
+    API_KEY = "AIzaSyDcjzzksf1mu9Hdd3otq4OcU0oug-vKIto";
+  };
   idx.extensions = [
     "svelte.svelte-vscode"
     "vue.volar"
